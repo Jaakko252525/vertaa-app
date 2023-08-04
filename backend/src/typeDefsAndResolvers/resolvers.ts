32e090a3d7c25878da5002f9530512eb8a6d0aa3
Jaakko252525 <124563002+Jaakko252525@users.noreply.github.com>
2023-08-04 18:58:37 +0300

    mongoose upgrades

--- backend/src/typeDefsAndResolvers/resolvers.ts
+++ backend/src/typeDefsAndResolvers/resolvers.ts
@@ -4,16 +4,61 @@
 // importing hardcoded data 
 import { sales } from "./typeDefs"
 
+// importing model
+import { ForSale } from "../models/ForSale";
 
 import mongoose from 'mongoose';
 
+// getForSale
 
+async function getForSale() {
+
+    try {
+        await mongoose.connect('mongodb+srv://MrRobots25:KFaQvEBfLrC76xNE@cluster.tt1mykg.mongodb.net/');
+        console.log('connected to database')
+
+        let data = ''        
+
+        // getting data from db
+        // @ts-ignore
+        await ForSale.find().then(result => {
+            data = result
+            
+        })
+        if (!data) {
+            console.log('find dont work', data)
+        }
+        return data
+
+
+
+} catch(error) {
+    console.log('error connecting to database!')
+    console.log()
+    console.log(error)
+}
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 async function main() {
 
     try {
         await mongoose.connect('mongodb+srv://MrRobots25:KFaQvEBfLrC76xNE@cluster.tt1mykg.mongodb.net/');
         console.log('connected to database')
+
+        let data = ''        
+
+        // getting data from db
+        // @ts-ignore
+        await ForSale.find().then(result => {
+            data = result
+            
+        })
+        if (!data) {
+            console.log('find dont work', data)
+        }
+        return data
+
 
 
 } catch(error) {
@@ -36,7 +81,10 @@ export const resolvers = {
 
     allSales: async () => {
         // connecting to mongoDB
-        main()
+        const data = await getForSale()
+        console.log('daata', data)
+
+        
         return sales
     }
   
