e90f99d88f8e67d6d33f6a9bbb4d3aedbd9dd135
Jaakko252525 <124563002+Jaakko252525@users.noreply.github.com>
2023-10-19 13:02:02 +0300

    chh

--- backend/src/scrapers/ToriScraper_3.ts
+++ backend/src/scrapers/ToriScraper_3.ts
@@ -5,10 +5,11 @@ import puppeteer from 'puppeteer';
 
 // function that searches tori.fi with parameter 
 export const browsing = async (item: string) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    userDataDir: './cache'
+  });
 
   const page = await browser.newPage();
-  page.setRequestInterception(true)
 
   await page.goto('https://www.tori.fi/koko_suomi?q=' + item);
 
