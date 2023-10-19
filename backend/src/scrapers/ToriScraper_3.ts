691dfbb6ac99cd29b10d772f21c5e0b24012e194
Jaakko252525 <124563002+Jaakko252525@users.noreply.github.com>
2023-10-19 13:10:26 +0300

    ff

--- backend/src/scrapers/ToriScraper_3.ts
+++ backend/src/scrapers/ToriScraper_3.ts
@@ -5,52 +5,58 @@ import puppeteer from 'puppeteer';
 
 // function that searches tori.fi with parameter 
 export const browsing = async (item: string) => {
-  const browser = await puppeteer.launch({
-    userDataDir: './cache'
-  });
-
-  const page = await browser.newPage();
-
-  await page.goto('https://www.tori.fi/koko_suomi?q=' + item);
-
-
-
-    // searching elements
     
-    const itemRowsSelector = '.item_row_flex';
+    try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto('https://www.tori.fi/koko_suomi?q=' + item);
 
 
-    let items = await page.$$eval(itemRowsSelector, (elements) => {
-        return elements.map(element => {
-          //@ts-ignore
-          const text = element.textContent.replace(/[\n\t]/g, ' ').trim();
-          return text;
+
+      // searching elements
+      
+      const itemRowsSelector = '.item_row_flex';
+
+
+      let items = await page.$$eval(itemRowsSelector, (elements) => {
+          return elements.map(element => {
+            //@ts-ignore
+            const text = element.textContent.replace(/[\n\t]/g, ' ').trim();
+            return text;
+          });
         });
-      });
 
+      
+
+      // extra spaces out
+      let count = 0
+      let newArrayOfItems = []
+
+      while (count < items.length) {
+
+        let item = items[count].replace(/\s+/g, " ");
+        newArrayOfItems.push(item)
+
+        count += 1
+      }
+
+
+
+      
+
+    // other actions...
+    await browser.close();
+
+    return newArrayOfItems
+  } 
+  catch(e) {
+    console.log('this is errorr:', e)
     
-
-    // extra spaces out
-    let count = 0
-    let newArrayOfItems = []
-
-    while (count < items.length) {
-
-      let item = items[count].replace(/\s+/g, " ");
-      newArrayOfItems.push(item)
-
-      count += 1
-    }
-
-
-
-    
-
-  // other actions...
-  await browser.close();
-
-  return newArrayOfItems
-
+  }
 
 }
 
